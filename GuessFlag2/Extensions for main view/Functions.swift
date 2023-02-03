0b4809bd10926f9d8ef8bbf807cc5efa417feac2
Нек <84968459+kidjuniper@users.noreply.github.com>
2023-02-04 02:41:14 +0300

    Update Functions.swift

--- GuessFlag2/Extensions for main view/Functions.swift
+++ GuessFlag2/Extensions for main view/Functions.swift
@@ -34,7 +34,7 @@ extension MainViewController {
         case 4: indicator.indicator3.layer.backgroundColor = UIColor.green.cgColor
         case 6: indicator.indicator2.layer.backgroundColor = UIColor.yellow.cgColor
         case 8: indicator.indicator1.layer.backgroundColor = UIColor.orange.cgColor
-        case 0: indicator.indicator4.layer.backgroundColor = UIColor.red.cgColor
+        case 0: indicator.indicator0.layer.backgroundColor = UIColor.red.cgColor
         default:
             if clickersButtons.count % 10 == 1 {
                 [indicator.indicator0, indicator.indicator1, indicator.indicator2, indicator.indicator3, indicator.indicator4].forEach{
